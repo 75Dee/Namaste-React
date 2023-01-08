d54cf0d94aa400a02555209cbc7f71d7b8609739
deepakporwal75@gmail.com <deepak_porwal@outlook.com>
2023-01-08 10:22:19 +0530

    code push

---
+++ index.js
@@ -0,0 +1,10 @@
+import React  from 'react'
+import ReactDOM  from 'react-dom'
+
+function Welcome(props) {
+    return <h1>Hello, {props.name}</h1>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const element = <Welcome name="Sara" />;
+root.render(element);
